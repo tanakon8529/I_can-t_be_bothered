535c67ea6b6f2dd8a243350e256b85f4e7f40732
tanakon8529 <tanakon.kabprapun@gmail.com>
2022-04-13 18:49:04 +0700

    basic and open browser, click join by img

--- core/main.py
+++ core/main.py
@@ -5,18 +5,21 @@ from loguru import logger
 from datetime import datetime
 import os
 
-newpath = r'log'
-real_path = os.path.realpath(newpath) 
+path_log = r'log'
+
+real_path_log = os.path.realpath(path_log)
 
 today = datetime.now().strftime("%Y%m%d")
 
-if not os.path.exists(real_path):
-    os.makedirs(real_path)
+if not os.path.exists(real_path_log):
+    os.makedirs(real_path_log)
 
-logger.add("{}/{}.log".format(real_path, today), enqueue=True, retention="30 days")
+
+
+logger.add("{}/{}.log".format(real_path_log, today), enqueue=True, retention="30 days")
 
 def process_run():
-    # url_input = "https://meet.google.com/nht-zaia-ygc"
-    # web = open_browser()
-    # web.open_browser(url_input)
-    print("test")
+
+    url_input = "https://meet.google.com/nht-zaia-ygc"
+    web = open_browser()
+    web.open_browser(url_input)
--- libs/browser_utill.py
+++ libs/browser_utill.py
@@ -1,13 +1,38 @@
 from loguru import logger
-from sys import platform
 
 import webbrowser
+import pyautogui
+import time
 import os
+
+
 
 class open_browser(object):
 
     def __init__(self):
-        self.url_web = ""
+        # create folder resource
+        path_resource = r'resource'
+        self.real_path_resource = os.path.realpath(path_resource) 
+        if not os.path.exists(self.real_path_resource):
+            os.makedirs(self.real_path_resource)
+
+        self.blabla = ""
+
+    def click_button_join(self):
+        try:
+            button_join = pyautogui.locateOnScreen(self.real_path_resource+'\join_class.png' , confidence=0.9)
+            time.sleep(2)
+            pyautogui.click(button_join)
+        except Exception as e:
+            logger.error("can't join class : {}".format(e)) 
+
+    def turn_off_mic_and_cam(self):
+        try:
+            time.sleep(5)
+            pyautogui.hotkey('ctrl', 'e')
+            pyautogui.hotkey('ctrl', 'd')
+        except Exception as e:
+            logger.error("can't turn off : {}".format(e)) 
 
     def open_browser(self, url_input):
         url = url_input
@@ -16,7 +41,12 @@ class open_browser(object):
         round_search_browser = 0
         try:
             while browser_enable == False:
-                browser_enable = webbrowser.open(url)
+                browser_enable = webbrowser.open(url, autoraise=False)
+
+                self.turn_off_mic_and_cam()
+                self.click_button_join()
+                logger.info("open : {}".format(url))
+
 
                 round_search_browser += 1
                 if round_search_browser == 3:
